2959287fc2cfb3722e5071f2782463c3929541ad
Sarah DeBey <sarah.debey@gmail.com>
2020-03-15 15:43:52 -0700

    add age module and tests for greet and age

--- hello-world-export/index.js
+++ hello-world-export/index.js
@@ -1,6 +1,9 @@
 'use strict';
 
 const greeting = require('./lib/greet.js');
+const age = require('./lib/age.js');
 
 greeting.sayHello();
 greeting.sayGoodbye();
+
+age.sayAge();
--- hello-world-export/lib/greet.js
+++ hello-world-export/lib/greet.js
@@ -2,13 +2,16 @@
 
 module.exports = exports = {};
 
-const name = process.argv[2];
+// const name = process.argv[2]
 
-exports.sayHello = function() {
+exports.sayHello = function(name) {
+  console.log('name', name);
   if (!name)
     throw new Error('no name given');
-  console.log('my name is ' + name);
+  return `my name is ${name}`;
 }
+
+
 
 exports.sayGoodbye = function() {
   console.log('tschuesss');
